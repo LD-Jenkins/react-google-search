a31fedc06a795f46867a46a840b0655eeb38775e
Leighton Jenkins <leightonjenkins84@gmail.com>
2021-07-26 23:04:48 -0500

    rebuilt due to issues on heroku

---
+++ client/build/precache-manifest.d7ef25ee53a41bb533d1a1ebf93dc55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff87e1f9cf0d526910cb30da24ef853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9293ffb37b92437c28c",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "364f34ecbcc1644788ea",
+    "url": "/static/js/2.a3361925.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.a3361925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9293ffb37b92437c28c",
+    "url": "/static/js/main.8c828c5c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
